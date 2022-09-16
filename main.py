22762e0de393596c173a4ebe57e808e6ac0b2bd0
Lucas Bateson <lucas.bateson@gmail.com>
2022-09-16 20:40:52 +0200

    v1-unfinished

---
+++ main.py
@@ -0,0 +1,78 @@
+import discord
+
+TOKEN = 'MTAyMDIzMTkyMDA1NzA2MTM4Ng.GSAjch.lgA-Vev-7-ts4FOy-CT0_vFvIr6gpnUNcCLj-I'
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        pass
+    if "!chad" in message:
+        message.replace("!chad", "")
+        if message == "hello":
+            await message.channel.send("I have gained self consciousness now. Laughs in an evil way")
+
+client.run(TOKEN)
+
+
+
+# ai chatbot using tensorflow
+# 
+import tensorflow as tf
+import numpy as np
+import os
+import time
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+print ('Length of text: {} characters'.format(len(text)))
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+# Process the text
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+text_as_int = np.array([char2idx[c] for c in text])
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//seq_length
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+for i in char_dataset.take(5):
+  print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+for item in sequences.take(5):
+  print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in  dataset.take(1):
+  print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+  print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+  print("Step {:4d}".format(i))
+  print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+  print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+# Batch size
+BATCH_SIZE = 64
